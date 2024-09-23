2ccd6b409a14a210896ad0d36e7b1bdb62d58ce4
Shahnawaz <ashahnawaz010@gmail.com>
2024-09-23 16:15:46 +0530

    pushed

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
 const SERVER_URL=import.meta.env.VITE_USER_API_END_POINT
-export const USER_API_END_POINT=`${SERVER_URL}/v1/user`;
-export const JOB_API_END_POINT=`${SERVER_URL}/v1/job`;
-export const APPLICATION_API_END_POINT=`${SERVER_URL}/v1/application`;
-export const COMPANY_API_END_POINT=`${SERVER_URL}/v1/company`;
+export const USER_API_END_POINT=`${SERVER_URL}/api/v1/user`;
+export const JOB_API_END_POINT=`${SERVER_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT=`${SERVER_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT=`${SERVER_URL}/api/v1/company`;
